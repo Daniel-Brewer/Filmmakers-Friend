b3c298332309cffddc3d92caae4b57b09aa8b7bd
Daniel Brewer <danielbrewer2014@comcast.net>
2019-02-13 20:43:41 -0600

    changed Back to Project button path

--- src/components/CastMember/CastMemberList.js
+++ src/components/CastMember/CastMemberList.js
@@ -16,11 +16,11 @@ export default class CastMemberList extends Component {
             <React.Fragment>
             <nav className="navbar navbar-light fixed-top light-blue flex-md-nowrap p-0 shadow">
                 <div className="logoutButton">
-                    <button onClick={() => {
-                        // change this to the right route eventually
-                        document.location.href = 'http://localhost:3000/projects'
-                    }}
-                        className="logoutButton">Back to Project</button>
+                <button type="button"
+                            onClick={()=> this.props.history.push(`/projects/${this.props.match.params.projectId}`)}
+                            className="btn btn-success">
+                        Back to Project
+                    </button>
                 </div>
             </nav>
             <h2>Cast</h2>
