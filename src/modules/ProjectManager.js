b5a340408a08bc7aa90df4a15201f346bca02397
Daniel Brewer <danielbrewer2014@comcast.net>
2019-02-14 13:40:14 -0600

    fixed get fetch in Program Manager, now edit fields are populated correctly

--- src/modules/ProjectManager.js
+++ src/modules/ProjectManager.js
@@ -3,7 +3,7 @@ const remoteURL = "http://localhost:5002"
 
 export default {
   get(id) {
-    return fetch(`${remoteURL}/projects/?user=${id}`).then(e => e.json())
+    return fetch(`${remoteURL}/projects/${id}`).then(p => p.json())
   },
   getAll() {
     let sessionUser=sessionStorage.getItem("credentials")
