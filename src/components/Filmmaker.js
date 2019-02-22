233234309e6f4711f854de4685036451bd96ea97
Daniel Brewer <danielbrewer2014@comcast.net>
2019-02-22 17:36:24 -0600

    added css to make p tag content bold font

--- src/components/Filmmaker.js
+++ src/components/Filmmaker.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import {BrowserRouter as Router} from 'react-router-dom'
 import ApplicationViews from "./ApplicationViews";
 import "./Filmmaker.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
+
 
 class Filmmaker extends Component {
   state = {
